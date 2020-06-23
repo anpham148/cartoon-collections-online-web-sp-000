3f895adc15af0b1c05093950b34882706c974fa1
An Pham <anpham148@gmail.com>
2020-06-23 12:50:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ end
 def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-   arr.include?("cheddar" || "gouda" || "camembert") ? arr[arr.index("cheddar" || "gouda" || "camembert")] 
+   arr.include?("cheddar" || "gouda" || "camembert") ? arr[arr.index("cheddar" || "gouda" || "camembert")] :  nil
 end
